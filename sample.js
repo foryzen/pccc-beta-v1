3edaff0b83b1ab896f00e055c7e5be2c8951e320
foryzen <92988541+foryzen@users.noreply.github.com>
2022-06-18 08:46:25 +0900

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,12 @@
+
+            
+            if(gpu==="AMD Radeon"){
+                $("#output-gpu").html("GPU")
+                $("#output-a-2").html("ASUS R7 250")
+            
+                var a2 = document.getElementById("output-a-2")
+                a2.href = a3z70Kvz[0]
+                var gpu_price = a3z70Kvz[1]
+            }
+
+//346
